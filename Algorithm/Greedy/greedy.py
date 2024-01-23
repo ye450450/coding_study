a84b786bf7744751f302e19428c06707cac5e8c0
ye450450 <104772331+ye450450@users.noreply.github.com>
2024-01-23 22:21:42 +0900

    greedy 1

--- Algorithm/Greedy/greedy.py
+++ Algorithm/Greedy/greedy.py
@@ -1,1 +1,15 @@
-import hi
+# 입력받기
+N, M, K = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+sum = 0
+count = 0
+for i in range(M):
+    if(count != 3):
+        sum += data[-1]
+        count += 1
+    else:
+        sum += data[-2]
+        count = 0
+print(sum)
